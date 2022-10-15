77308cae7c3efd2018f3fa6443830d5d4bad835f
raman mhedin <92646002+ramanmhedin@users.noreply.github.com>
2022-10-15 21:07:10 +0300

    add variable and constructor

--- Staff.java
+++ Staff.java
@@ -4,5 +4,30 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class Staff {
+    private Integer id;
+    private String name;
+    private String gender;
+    private String position;
+    private Date dob;
+    private Integer phonenumber;
+    private String email;
+    private Double salary;
+    private String address;
+    private String password;
+    Scanner input = new Scanner(System.in);
+
+    public Staff(Integer id, String name, String gender, String position, String dob, Integer phonenumber, String email,
+            Double salary, String address, String password) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.position = position;
+        this.dob = convertStringToDate(dob);
+        this.phonenumber = phonenumber;
+        this.email = email;
+        this.salary = salary;
+        this.address = address;
+        this.password = password;
+    }
     
 }
