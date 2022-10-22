0c0d8141cd901ea9d0aa3cfc412ec3c0fc957c75
raman mhedin <92646002+ramanmhedin@users.noreply.github.com>
2022-10-22 19:51:35 +0300

    adding InvoiceController class

---
+++ Controllers/InvoiceController.java
@@ -0,0 +1,46 @@
+package Controller;
+
+import Models.GarmentModel;
+import Models.InvoiceModel;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+// String cashierName, int cashierID, int invoiceID, Date date, ArrayList<GarmentModel> garmentsList, double totalPrice, double totalPricePerType
+
+public abstract class InvoiceController {
+
+    private static ArrayList<InvoiceModel> invoiceBodyList = new ArrayList<InvoiceModel>();
+    
+    public InvoiceController(InvoiceModel invoice) {
+    	
+    }
+    
+    public static void addNewInvoice(InvoiceModel Invoice){
+        invoiceBodyList.add(Invoice);
+    }
+    
+    public static void removeInvoice(InvoiceModel Invoice){
+        invoiceBodyList.remove(Invoice);
+    }
+    
+    public double calculateTotalPrice(ArrayList<GarmentModel> garmentsList) {
+		double totalPrice = 0;
+		for(int i = 0; i < garmentsList.size(); i++) {
+			totalPrice = totalPrice + garmentsList.get(i).getPrice();
+		}
+		
+		return totalPrice;
+	}
+    
+    public double calculateSameGarmentTotal(ArrayList<GarmentModel> garmentsList) {
+    	double totalPriceForType = 0;
+		for(int i = 0; i < garmentsList.size(); i++) {
+			totalPriceForType = garmentsList.get(i).getPrice() * garmentsList.get(i).getQuantity();
+		}
+		
+		return totalPriceForType;
+    }
+    
+}
+
