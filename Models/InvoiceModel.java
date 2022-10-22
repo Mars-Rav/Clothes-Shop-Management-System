c4f1971c5d3abf926979d23acf373d00c524fc2e
HasanSabah25 <92628172+HasanSabah25@users.noreply.github.com>
2022-10-22 23:18:39 +0300

    Adding TotalPrice Field for the getter and setter

--- Models/InvoiceModel.java
+++ Models/InvoiceModel.java
@@ -8,9 +8,19 @@ import java.util.Date;
 
 public class InvoiceModel extends InvoiceHeader {
 	
-	public InvoiceModel(String cashierName, int cashierID, int invoiceID, Date date, ArrayList<GarmentModel> garmentsList) {
+	private Double totalPrice;
+	
+	public InvoiceModel(String cashierName, int cashierID, int invoiceID, Date date, ArrayList<GarmentModel> garmentsList,double totalPrice) {
 		super(cashierName, cashierID, invoiceID, date);
-		
+		setTotalPrice(totalPrice);
+	}
+	
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	
+	public Double getTotalPrice() {
+		return totalPrice;
 	}
 
 }
