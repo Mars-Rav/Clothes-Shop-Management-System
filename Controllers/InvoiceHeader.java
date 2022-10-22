c8e19de7a7d13b83c146eecf610a66a6c9d5706a
MabastAbdulqadir <92991683+MabastAbdulqadir@users.noreply.github.com>
2022-10-22 22:22:55 +0300

    Update InvoiceHeader.java

--- Controllers/InvoiceHeader.java
+++ Controllers/InvoiceHeader.java
@@ -1,1 +1,14 @@
+package Controller;
 
+import java.util.Date;
+import Models.InvoiceHeader;
+import java.util.ArrayList;
+
+public abstract class InvoiceHeaderController {
+	
+    public InvoiceHeaderController(String cashierName, int cashierID, int invoiceID) {
+
+    	InvoiceHeader header = new InvoiceHeader(cashierName, cashierID, invoiceID, new Date());
+    }
+
+}
