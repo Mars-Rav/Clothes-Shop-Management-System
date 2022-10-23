fcc60790fe9f3e864186d07a10fef65ee5cc1da6
Mina Bassam <88538757+Mars-Rav@users.noreply.github.com>
2022-10-23 10:14:12 +0300

    Update Manager.java

--- Controllers/Manager.java
+++ Controllers/Manager.java
@@ -8,12 +8,16 @@ public abstract class Manager{
 		GarmentController.addToInventory(new GarmentModel(id, name, barcode, brand, size, color, dateAdded, model, price, quantity, category, discount, description));
 	}
 	
-	public void removeGarment(int option, String name, int id) {
-		GarmentController.removeFromInventory(option, name, id);
+	public void removeGarment(String name) {
+		GarmentController.removeGarment(name);
 	}
 	
-	public void addNewCashier(int id, String name, String gender, String dob, String phoneNumber, String email, double salary, String address, String password, String role) {
-		CashierController.addNewCashier(new User(id, name, gender, dob, phoneNumber, email, salary, address, password, role));
+	public void removeGarment(int id) {
+		GarmentController.removeGarment(id);
+	}
+	
+	public void addNewCashier(int id, String name, String gender, String position, String dob, String phoneNumber, String email, double salary, String address, String password, String role) {
+		CashierController.addNewCashier(new User(id, name, gender, position, dob, phoneNumber, email, salary, address, password, role));
 	}
 	
 	public void removeCashier(String name) {
@@ -25,3 +29,4 @@ public abstract class Manager{
 	}
 
 }
+
