ef2c6ae77d3c21ba17bf9494c471a2f01b4d64ab
srushtnuri <95306747+srushtnuri@users.noreply.github.com>
2022-10-23 09:04:20 +0300

    Update GarmentController.java

--- Controllers/GarmentController.java
+++ Controllers/GarmentController.java
@@ -14,30 +14,24 @@ public abstract class GarmentController {
 				
 	}
 	
-	public static void removeFromInventory(int option, String name, int id) {
-		
-		switch(option){
-			case 1:
-				for(int i = 0; i < inventory.size(); i++){
-					
-					if(inventory.get(i).getName().contains(name)){
-						inventory.remove(inventory.get(i));
-					}
-				}
+	public static void removeGarment(String name){
+    	
+		for(int i = 0; i < inventory.size(); i++){
 				
-				break;
-				
-			case 2:
-				for(int i = 0; i < inventory.size(); i++){
-					
-					if(inventory.get(i).getID() == id){
-						inventory.remove(inventory.get(i));
-					}
-					
-				}
-				
-				break;	
+			if(inventory.get(i).getName().contains(name)){
+				inventory.remove(inventory.get(i));
+			}
+		}  
+    }
+    
+    public static void removeGarment(int id) {
+    	for(int i = 0; i < inventory.size(); i++){
+			
+			if(inventory.get(i).getID() == id){
+				inventory.remove(inventory.get(i));
+			}
+			
 		}
-	}
+    }
 	
 }
